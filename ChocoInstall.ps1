42703c2a5e22cc211bef488a4d0af361c624aab4
Michael Mardahl <iphase@iphase.dk>
2019-10-12 01:37:51 +0200

    Update ChocoInstall.ps1
    Added -usermode switch to enable non-administrative installers

--- ChocoInstall.ps1
+++ ChocoInstall.ps1
@@ -1,4 +1,3 @@
-#requires -runasadministrator
 #requires -version 5.0
 <#
 .SYNOPSIS
@@ -6,22 +5,29 @@ Script to install chocolatey packages via Intune Win32App
 .DESCRIPTION
 Combining this script with the power of Win32App deployment via Intune, you can use the official Chcolatey package repo to install applications
 .REQUIREMENTS
-This script must be run from the context of the SYSTEM account.
+This script must be run from the context of the SYSTEM account, unless -usermode switch is specified.', in which case it can be run as a non-admin.
 Designed to be run by Intune or SCCM Agent.
 .EXAMPLE
 Installing greenshot via intune Win32App
     Install command: powershell -ex bypass -file ChocoInstall.ps1 -package greenshot
+    .EXAMPLE
+Installing greenshot via intune Win32App in "user mode" (for apps that install into APPDATA)
+    Install command: powershell -ex bypass -file ChocoInstall.ps1 -package greenshot -usermode
 .EXAMPLE
 Upgrading greenshot via intune Win32App
     Install command: powershell -ex bypass -file ChocoInstall.ps1 -package greenshot -upgrade
 .EXAMPLE
 Uninstalling greenshot via intune Win32App
     Install command: powershell -ex bypass -file ChocoInstall.ps1 -package greenshot -uninstall
+.NOTES
+The usermode switch must be used on upgrade and uninstall packages as well, if the app was originally deployed using this switch.
+The switch essetially installs a seperate Chocolatey installation for each user it is deployed to, seperate of the machine wide chocolatey install.
+This switch will cause up to a one minute delay in the unattended installation because of warning timeouts the chocolatey enforces on non-admins.
 .COPYRIGHT
 MIT License, feel free to distribute and use as you like, please leave author information.
 .AUTHOR
 Michael Mardahl - @michael_mardahl on twitter - BLOG: https://www.iphase.dk
-Greenshot install code borrowed from https://www.petervanderwoude.nl/post/combining-the-powers-of-the-intune-management-extension-and-chocolatey/
+Chocolatey install code inspired by https://www.petervanderwoude.nl/post/combining-the-powers-of-the-intune-management-extension-and-chocolatey/
 .DISCLAIMER
 This script is provided AS-IS, with no warranty - Use at own risk!
 #>
@@ -33,28 +39,72 @@ PARAM(
     [Parameter(Mandatory=$false)]
         [switch]$uninstall,
     [Parameter(Mandatory=$false)]
+        [switch]$usermode,
+    [Parameter(Mandatory=$false)]
         [switch]$upgrade
 )
 
-#Path to Chocolatey
-$ChocoInstall = Join-Path ([System.Environment]::GetFolderPath("CommonApplicationData")) "Chocolatey\bin\choco.exe"
+######
+#
+# CHOCOLATEY INSTALL CHECKS
+#
+######
 
-#Verify Chocolatey is installed, otherwise try and install from official repository.
-if(!(Test-Path $ChocoInstall)) {
+# Skip download of 7z components and use Windows built-in compression
+$env:chocolateyUseWindowsCompression = 'true'
+
+if ($usermode) {
+
+    # Trying to get around possible restrictions
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    # Override Chocolatey install path to enable user-mode installs (those pesky things that only install into the users APPDATA folder)
+    $ChocolateyInstall = "$($env:APPDATA)\chocolatey"
+    New-Item $ChocolateyInstall -Type Directory -Force
+    $env:ChocolateyInstall = $ChocolateyInstall
+    [Environment]::SetEnvironmentVariable("ChocolateyInstall", $ChocolateyInstall, "User")
+
+}else{
+
+    # Default path to Chocolatey (machine wide)
+    $ChocolateyInstall = "$($env:ProgramData)\Chocolatey" #\bin\choco.exe
+
+}
+
+# Build path to executeable
+$Choco = "$ChocolateyInstall\bin\choco.exe"
+
+# Verify that Chocolatey is installed, otherwise try and install from the official repository.
+if(!(Test-Path $Choco)) {
      try {
-         Invoke-Expression ((New-Object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
+         Invoke-Expression ((New-Object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction stop
      }
      catch {
          Throw “Failed to install Chocolatey”
      }       
+} else {
+    # Try and upgrade chocolatey if it was already installed
+    Write-Output "Trying to upgrade Chocolatey..."
+    try {
+        Invoke-Expression “cmd.exe /c $Choco upgrade chocolatey -y” -ErrorAction stop
+    }
+    catch {
+        Write-Output “Failed to auto upgrade chocolatey”
+    }
 }
 
-#Determine if we are doing either Install or Uninstall
+
+######
+#
+# PACKAGE HANDLING
+#
+######
+
+# Determine if we are doing either Install or Uninstall
 if($uninstall) {
 
-    #Uninstall requested package
+    # Uninstall requested package
     try {
-        Invoke-Expression “cmd.exe /c $ChocoInstall uninstall $package -y” -ErrorAction Stop
+        Invoke-Expression “cmd.exe /c $Choco uninstall $package -y” -ErrorAction Stop
     }
     catch {
         Throw “Failed to uninstall $package”
@@ -62,9 +112,9 @@ if($uninstall) {
 
 } elseif($upgrade) {
 
-    #Upgrade requested package to latest approved version
+    # Upgrade requested package to latest approved version
     try {
-        Invoke-Expression “cmd.exe /c $ChocoInstall upgrade $package -y” -ErrorAction Stop
+        Invoke-Expression “cmd.exe /c $Choco upgrade $package -y” -ErrorAction Stop
     }
     catch {
         Throw “Failed to upgrade $package”
@@ -72,9 +122,9 @@ if($uninstall) {
 
 } else {
 
-    #Install requested package
+    # Install requested package
     try {
-        Invoke-Expression “cmd.exe /c $ChocoInstall Install $package -y” -ErrorAction Stop
+        Invoke-Expression “cmd.exe /c $Choco Install $package -y” -ErrorAction Stop
     }
     catch {
         Throw “Failed to install $package”
